a635fcac78d87644a69d80944e377f81ac3a7ae7
jpjohnson <johnpjohnson@gmail.com>
2015-11-12 09:57:28 -0500

    Update buildAndZip.sh

--- buildAndZip.sh
+++ buildAndZip.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# build for osx, linux and windows
+# build for osx, linux and windows and bundle
 # 
-# run
+# run to get zip
 # > ./buildAndZip.sh app
+# out : app.zip
 #
+# run to get tar
+# > ./buildAndZip.sh app tar
+# out : app.tar.gz
+
 env GOOS=darwin GOARCH=amd64 go build
 mv $1 $1_osx
 env GOOS=linux GOARCH=amd64 go build
 mv $1 $1_linux
 env GOOS=windows GOARCH=amd64 go build
-zip -r $1_bundle.zip $1_osx $1_linux $1.exe index.html
+
+if [ $2 = "tar" ]; then
+  tar -cvf $1_bundle.tar.gz $1_osx $1_linux $1.exe 
+else
+  zip -r $1_bundle.zip $1_osx $1_linux $1.exe
+fi
